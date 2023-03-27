8af5a1f3a083778a3f9208313da0bcbcd953b30a
TapeMate <admin@tapemate.onmicrosoft.com>
2023-03-27 11:38:07 +0200

    added dummy content for AboutView.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>About</h3>
+    <h5>Angaben zum Sitz des Unternehmens</h5>
+    <p>Muster GmbH & Co. KG</p>
+    <p>Musterstraße 123</p>
+    <p>12345 Musterstadt</p>
+    <p>E-Mail: Info@dummy.de</p>
+    <p>Telefon: 01234 555 666 777</p>
+    <h5>Geschäftsführung</h5>
+    <p>Hans Mustermann</p>
+    <p>Thomas Mustermann</p>
+    <p>Sabine Mustermann</p>
+    <h5>Aufsichtsratsvorsitzender</h5>
+    <p>Sabine Mustermann</p>
+    <h5>Sitz der Gesellschaft: Musterstadt, Deutschland</h5>
+    <p>Registergericht Musterstadt</p>
+    <p>HRB 123456</p>
+    <p>Ust.-ID.: DE123456789</p>
+    <h5>Internet: https://muster.de</h5>
   </div>
 </template>
+
+<style scoped>
+.about {
+  padding: 1rem;
+}
+h3 {
+  padding: 2rem;
+  font-weight: bold;
+}
+</style>
