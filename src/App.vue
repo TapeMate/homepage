b2a2b4c03e64e46328f59ec84c2e36fcb520655f
TapeMate <admin@tapemate.onmicrosoft.com>
2023-03-27 11:01:48 +0200

    added NavbarMain component. Integrated Bootstrap cdn.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
+  <NavbarMain />
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
 </template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import NavbarMain from "./components/NavbarMain.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    NavbarMain,
+  },
+};
+</script>
 
 <style>
 #app {
