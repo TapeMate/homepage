e9ed1ddd4d1d763d6ad22395ac8740a01db95605
TapeMate <admin@tapemate.onmicrosoft.com>
2023-03-28 12:42:25 +0200

    added data to about.

---
+++ src/components/About.vue
@@ -0,0 +1,48 @@
+<template>
+  <h5>Angaben zum Sitz des Unternehmens</h5>
+  <p>{{ company.name }}</p>
+  <p>{{ company.street }}</p>
+  <p>{{ company.zipCode }} {{ company.city }}</p>
+  <p>E-Mail: {{ company.email }}</p>
+  <p>Telefon: {{ company.phone }}</p>
+
+  <h5>Geschäftsführung</h5>
+  <p>{{ ceo.ceo01 }}</p>
+  <p>{{ ceo.ceo02 }}</p>
+  <p>{{ ceo.ceo03 }}</p>
+  <h5>Aufsichtsratsvorsitzender</h5>
+  <p>{{ ceo.executive }}</p>
+  <h5>Sitz der Gesellschaft: {{ company.city }}, {{ company.country }}</h5>
+  <p>Registergericht {{ company.city }}</p>
+  <p>HRB {{ company.registerID }}</p>
+  <p>Ust.-ID.: {{ company.taxID }}</p>
+  <h5>Internet: {{ company.homepage }}</h5>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      company: {
+        name: "Muster GmbH & Co. KG",
+        street: "Musterstraße 123",
+        zipCode: "12345",
+        city: "Musterstadt",
+        country: "Deutschland",
+        email: "company@muster-firma.de",
+        homepage: "https://muster-firma.de",
+        phone: "01234 555 666 777",
+        registerID: "123456",
+        taxID: "DE123456789",
+      },
+      ceo: {
+        ceo01: "Hans Mustermann",
+        ceo02: "Thomas Mustermann",
+        ceo03: "Sabine Mustermann",
+        executive: "Sabine Mustermann",
+      },
+    };
+  },
+};
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="about">
     <h3>About</h3>
-    <h5>Angaben zum Sitz des Unternehmens</h5>
-    <p>Muster GmbH & Co. KG</p>
-    <p>Musterstraße 123</p>
-    <p>12345 Musterstadt</p>
-    <p>E-Mail: Info@dummy.de</p>
-    <p>Telefon: 01234 555 666 777</p>
-    <h5>Geschäftsführung</h5>
-    <p>Hans Mustermann</p>
-    <p>Thomas Mustermann</p>
-    <p>Sabine Mustermann</p>
-    <h5>Aufsichtsratsvorsitzender</h5>
-    <p>Sabine Mustermann</p>
-    <h5>Sitz der Gesellschaft: Musterstadt, Deutschland</h5>
-    <p>Registergericht Musterstadt</p>
-    <p>HRB 123456</p>
-    <p>Ust.-ID.: DE123456789</p>
-    <h5>Internet: https://muster.de</h5>
+    <About />
   </div>
 </template>
+
+<script>
+import About from "../components/About.vue";
+
+export default {
+  components: {
+    About,
+  },
+};
+</script>
 
 <style scoped>
 .about {
