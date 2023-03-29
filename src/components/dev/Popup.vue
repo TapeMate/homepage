57e61b140a6ec21fc18dee0bcf8de6f8bc5767c8
TapeMate <admin@tapemate.onmicrosoft.com>
2023-03-29 12:57:45 +0200

    build and tested dev component for popups.

--- src/components/dev/Popup.vue
+++ src/components/dev/Popup.vue
@@ -2,7 +2,7 @@
   <div class="popup">
     <div class="popup-inner">
       <slot />
-      <button class="popup-close">Close Popup</button>
+      <button class="popup-close" @click="TogglePopup()">Close Popup</button>
     </div>
   </div>
 </template>
@@ -10,7 +10,26 @@
 <script>
 export default {
   name: "Popup",
+  props: ["TogglePopup"],
 };
 </script>
 
-<style></style>
+<style scoped>
+.popup {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 99;
+  background: rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.popup-inner {
+  background: white;
+  padding: 32px;
+}
+</style>
--- src/components/dev/Popups.vue
+++ src/components/dev/Popups.vue
@@ -1,21 +1,52 @@
 <template>
   <main>
     <h1>Vue Popups</h1>
-    <button>Open Popup</button>
-    <Popup>
-      <h2>MyPopup</h2>
+    <button @click="() => TogglePopup('buttonTrigger')">Open Popup</button>
+    <Popup
+      v-if="popupTriggers.buttonTrigger"
+      :TogglePopup="() => TogglePopup('buttonTrigger')"
+    >
+      <h2>My Button Popup</h2>
+    </Popup>
+    <Popup
+      v-if="popupTriggers.timedTrigger"
+      :TogglePopup="() => TogglePopup('timedTrigger')"
+    >
+      <h2>My Timed Popup</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+        quisquam, expedita similique iure ipsa corporis doloremque sequi
+        inventore nulla delectus unde id dolorum modi labore consectetur
+        dolores! Beatae, facere reiciendis!
+      </p>
     </Popup>
   </main>
 </template>
 
 <script>
+import { ref } from "vue";
 import Popup from "./Popup";
 
 export default {
   name: "Popups",
   setup() {
+    const popupTriggers = ref({
+      buttonTrigger: false,
+      timedTrigger: false,
+    });
+
+    const TogglePopup = (trigger) => {
+      popupTriggers.value[trigger] = !popupTriggers.value[trigger];
+    };
+
+    setTimeout(() => {
+      popupTriggers.value.timedTrigger = true;
+    }, 3000);
+
     return {
       Popup,
+      popupTriggers,
+      TogglePopup,
     };
   },
 };
