68fdfd904237591cbb2b203b963a2544e58e8312
TapeMate <admin@tapemate.onmicrosoft.com>
2023-04-02 13:28:06 +0200

    changed data structure from about.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,52 +1,38 @@
 <template>
   <h5>Angaben zum Sitz des Unternehmens</h5>
-  <p>{{ company.name }}</p>
-  <p>{{ company.street }}</p>
-  <p>{{ company.zipCode }} {{ company.city }}</p>
-  <p v-if="company.email.length > 0">E-Mail: {{ company.email }}</p>
-  <p v-if="company.phone.length > 0">Telefon: {{ company.phone }}</p>
+  <p>{{ companyData[0].name }}</p>
+  <p>{{ companyData[0].street }}</p>
+  <p>{{ companyData[0].zipCode }} {{ companyData[0].city }}</p>
+  <p v-if="companyData[0].email != ''">E-Mail: {{ companyData[0].email }}</p>
+  <p v-if="companyData[0].phone != ''">Telefon: {{ companyData[0].phone }}</p>
 
   <h5>Geschäftsführung</h5>
-  <p>{{ ceo.ceo01 }}</p>
-  <p v-if="ceo.ceo02.length > 0">{{ ceo.ceo02 }}</p>
-  <p v-if="ceo.ceo03.length > 0">{{ ceo.ceo03 }}</p>
+  <p>{{ companyData[0].ceo01 }}</p>
+  <p v-if="companyData[0].ceo02 != ''">{{ companyData[0].ceo02 }}</p>
+  <p v-if="companyData[0].ceo03 != '' > 0">{{ companyData[0].ceo03 }}</p>
 
-  <h5 v-if="ceo.executive.length > 0">Aufsichtsratsvorsitzender</h5>
-  <p v-if="ceo.executive.length > 0">{{ ceo.executive }}</p>
+  <h5 v-if="companyData[0].executive != ''">Aufsichtsratsvorsitzender</h5>
+  <p v-if="companyData[0].executive != ''">
+    {{ companyData[0].executive }}
+  </p>
 
-  <h5>Sitz der Gesellschaft: {{ company.city }}, {{ company.country }}</h5>
-  <p>Registergericht {{ company.city }}</p>
-  <p>HRB {{ company.registerID }}</p>
-  <p>Ust.-ID.: {{ company.taxID }}</p>
-  <h5 v-if="company.homepage.length > 0">
-    Internet: <a :href="company.homepage">Besuchen Sie uns!</a>
+  <h5>
+    Sitz der Gesellschaft: {{ companyData[0].city }},
+    {{ companyData[0].country }}
+  </h5>
+  <p>Registergericht {{ companyData[0].city }}</p>
+  <p>HRB {{ companyData[0].registerID }}</p>
+  <p>Ust.-ID.: {{ companyData[0].taxID }}</p>
+  <h5 v-if="companyData[0].homepage != ''">
+    Internet: <a :href="companyData[0].homepage">Besuchen Sie uns!</a>
   </h5>
 </template>
 
 <script>
 export default {
   name: "About",
-  data() {
-    return {
-      company: {
-        name: "Muster GmbH & Co. KG",
-        street: "Musterstraße 123",
-        zipCode: "12345",
-        city: "Musterstadt",
-        country: "Deutschland",
-        email: "company@muster-firma.de",
-        homepage: "https://muster-firma.de",
-        phone: "01234 555 666 777",
-        registerID: "123456",
-        taxID: "DE123456789",
-      },
-      ceo: {
-        ceo01: "Hans Mustermann",
-        ceo02: "Thomas Mustermann",
-        ceo03: "Sabine Mustermann",
-        executive: "Sabine Mustermann",
-      },
-    };
+  props: {
+    companyData: Array,
   },
 };
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="about">
     <h3>About</h3>
-    <About />
+    <About :companyData="companyData" />
   </div>
 </template>
 
 <script>
 import About from "../components/About.vue";
+import config from "../assets/json/config.json";
 
 export default {
   components: {
     About,
+  },
+  data() {
+    return {
+      companyData: config.companyInfo,
+    };
   },
 };
 </script>
