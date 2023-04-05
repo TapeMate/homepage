60de632c4de81675890b8e5014a369ee7981954e
TapeMate <admin@tapemate.onmicrosoft.com>
2023-04-06 01:14:42 +0200

    implemented sass. linked new css file to index.html

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,118 +1,120 @@
 <template>
-  <div v-if="this.screenOut === true" class="success-message">
-    <h2>Contact Request has been send successfully!</h2>
-    <i class="fa-solid fa-envelope-circle-check fa-bounce"></i>
-  </div>
-  <div @keyup="enableSubmit" class="form-container">
-    <form @submit="onSubmit">
-      <div class="input-control" style="width: 25%">
-        <label for="topic">Topic</label>
-        <div class="success-icon-wrapper">
-          <input
-            @keyup="validateTopic"
+  <div class="contact-form-container">
+    <div v-if="this.screenOut === true" class="success-message">
+      <h2>Contact Request has been send successfully!</h2>
+      <i class="fa-solid fa-envelope-circle-check fa-bounce"></i>
+    </div>
+    <div @keyup="enableSubmit" class="form-container">
+      <form @submit="onSubmit">
+        <div class="input-control" style="width: 25%">
+          <label for="topic">Topic</label>
+          <div class="success-icon-wrapper">
+            <input
+              @keyup="validateTopic"
+              type="text"
+              v-model="topic"
+              name="topic"
+              id="topic"
+              placeholder="Type in your topic here"
+              :class="checkTopic === true ? 'success' : ''"
+            />
+            <i
+              v-if="checkTopic === true"
+              class="fa-regular fa-circle-check"
+              id="topic-success"
+            ></i>
+          </div>
+          <p class="topic-error" v-if="errorTopic">
+            {{ errorTopic }}
+          </p>
+        </div>
+        <div class="input-control">
+          <label for="description">Description</label>
+          <textarea
+            @keyup="validateDescription"
             type="text"
-            v-model="topic"
-            name="topic"
-            id="topic"
+            v-model="description"
+            name="description"
+            id="description"
             placeholder="Type in your topic here"
-            :class="checkTopic === true ? 'success' : ''"
           />
-          <i
-            v-if="checkTopic === true"
-            class="fa-regular fa-circle-check"
-            id="topic-success"
-          ></i>
+          <p class="description-error" v-if="errorDescription">
+            {{ errorDescription }}
+          </p>
         </div>
-        <p class="topic-error" v-if="errorTopic">
-          {{ errorTopic }}
-        </p>
-      </div>
-      <div class="input-control">
-        <label for="description">Description</label>
-        <textarea
-          @keyup="validateDescription"
-          type="text"
-          v-model="description"
-          name="description"
-          id="description"
-          placeholder="Type in your topic here"
-        />
-        <p class="description-error" v-if="errorDescription">
-          {{ errorDescription }}
-        </p>
-      </div>
-      <div class="input-container">
-        <div class="input-control">
-          <label for="phone">Phone</label>
+        <div class="input-container">
           <div class="input-control">
-            <div class="success-icon-wrapper">
-              <input
-                @keyup="validatePhoneNumber"
-                type="text"
-                v-model="phoneNumber"
-                name="phone"
-                id="phone"
-                placeholder="Enter your phonenumber"
-                :class="checkPhone === true ? 'success' : ''"
-              />
-              <i
-                v-if="checkPhone === true"
-                class="fa-regular fa-circle-check"
-                id="phone-success"
-              ></i>
+            <label for="phone">Phone</label>
+            <div class="input-control">
+              <div class="success-icon-wrapper">
+                <input
+                  @keyup="validatePhoneNumber"
+                  type="text"
+                  v-model="phoneNumber"
+                  name="phone"
+                  id="phone"
+                  placeholder="Enter your phonenumber"
+                  :class="checkPhone === true ? 'success' : ''"
+                />
+                <i
+                  v-if="checkPhone === true"
+                  class="fa-regular fa-circle-check"
+                  id="phone-success"
+                ></i>
+              </div>
             </div>
-          </div>
-          <p class="phone-error" v-if="errorPhone">
-            {{ errorPhone }}
-          </p>
-        </div>
-        <div class="input-control">
-          <label for="email">E-mail Adress</label>
-          <div class="success-icon-wrapper">
-            <input
-              @keyup="validateMailAdress"
-              type="email"
-              v-model="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email adress"
-              :class="checkMail === true ? 'success' : ''"
-            />
-            <i
-              v-if="checkMail === true"
-              class="fa-regular fa-circle-check"
-              id="mail-success"
-            ></i>
-          </div>
-          <p class="mail-error" v-if="errorMessageMail">
-            {{ errorMessageMail }}
-          </p>
-        </div>
-        <div class="input-control">
-          <label for="email-repeat">E-mail Verification</label>
-          <div class="success-icon-wrapper">
-            <input
-              @keyup="verifyMailAdress"
-              type="email"
-              v-model="verifyEmail"
-              name="email-repeat"
-              id="email-repeat"
-              placeholder="Repeat your email adress"
-              :class="checkMailRepeat === true ? 'success' : ''"
-            />
-            <i
-              v-if="checkMailRepeat === true"
-              class="fa-regular fa-circle-check"
-              id="mail-repeat-success"
-            ></i>
-            <p class="mail-repeat-error" v-if="errorMailRepeat">
-              {{ errorMailRepeat }}
+            <p class="phone-error" v-if="errorPhone">
+              {{ errorPhone }}
             </p>
           </div>
+          <div class="input-control">
+            <label for="email">E-mail Adress</label>
+            <div class="success-icon-wrapper">
+              <input
+                @keyup="validateMailAdress"
+                type="email"
+                v-model="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email adress"
+                :class="checkMail === true ? 'success' : ''"
+              />
+              <i
+                v-if="checkMail === true"
+                class="fa-regular fa-circle-check"
+                id="mail-success"
+              ></i>
+            </div>
+            <p class="mail-error" v-if="errorMessageMail">
+              {{ errorMessageMail }}
+            </p>
+          </div>
+          <div class="input-control">
+            <label for="email-repeat">E-mail Verification</label>
+            <div class="success-icon-wrapper">
+              <input
+                @keyup="verifyMailAdress"
+                type="email"
+                v-model="verifyEmail"
+                name="email-repeat"
+                id="email-repeat"
+                placeholder="Repeat your email adress"
+                :class="checkMailRepeat === true ? 'success' : ''"
+              />
+              <i
+                v-if="checkMailRepeat === true"
+                class="fa-regular fa-circle-check"
+                id="mail-repeat-success"
+              ></i>
+              <p class="mail-repeat-error" v-if="errorMailRepeat">
+                {{ errorMailRepeat }}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <button type="submit" id="submit" disabled="disabled">Submit</button>
-    </form>
+        <button type="submit" id="submit" disabled="disabled">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -296,179 +298,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.success-icon-wrapper {
-  position: relative;
-}
-#topic-success {
-  position: absolute;
-  display: inline;
-  color: rgb(0, 187, 0);
-  top: 28%;
-  right: 2px;
-}
-
-#phone-success,
-#mail-success,
-#mail-repeat-success {
-  position: absolute;
-  display: inline;
-  color: rgb(0, 187, 0);
-  top: 28%;
-  right: 9px;
-}
-.success-message {
-  position: absolute;
-  z-index: 1;
-  display: flex;
-  justify-content: space-around;
-  flex-wrap: wrap;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  color: lime;
-  background: rgba(0, 0, 0, 0.9);
-}
-.success-message h2 {
-  width: 100%;
-  height: 25%;
-}
-.success-message i {
-  font-size: 100px;
-  margin-top: -900px;
-}
-.error {
-  background: #fdebeb !important;
-  /* color: rgb(165, 0, 0) !important; */
-  outline: 1px solid red !important;
-}
-.success {
-  background: rgb(209, 255, 209);
-  color: rgb(0, 85, 0);
-}
-
-.phone-error,
-.mail-error,
-.mail-repeat-error,
-.topic-error,
-.description-error {
-  padding: 0.5rem;
-  font-size: 12px;
-  margin: 2px;
-  left: 65%;
-  background: #ffe3e3;
-  border: 1px solid;
-  color: rgb(255, 0, 0);
-  display: inline;
-}
-#phone {
-  width: 20%;
-}
-.form-entry-info {
-  background: rgba(255, 255, 255, 0.6);
-  padding: 1rem 0;
-  margin-bottom: 30px;
-  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.5);
-}
-.form-container {
-  padding: 4% 10% 2%;
-  background-image: url("../assets/slider/slider02.jpg");
-  background-size: cover;
-}
-.input-container {
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr;
-}
-input,
-label {
-  width: 100%;
-  display: inline-block;
-  padding: 0.5rem;
-  margin: 3px;
-}
-input {
-  padding: 0.5rem 1rem;
-  min-width: 200px;
-}
-textarea {
-  width: 100%;
-  height: 200px;
-  display: inline-block;
-  padding: 1rem;
-  margin: 3px;
-}
-.input-container label {
-  text-align: center;
-  height: auto;
-  max-width: max-content;
-  min-width: 120px;
-}
-.input-container .input-control {
-  display: flex;
-  justify-content: left;
-  width: 100%;
-}
-.input-container .input-control:nth-child(2) {
-  margin-bottom: 0px !important;
-}
-.input-container .input-control label,
-.input-container .input-control input {
-  width: 20%;
-}
-.input-control {
-  margin-bottom: 15px;
-}
-input,
-textarea,
-label {
-  border: none;
-  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.5);
-  outline: none;
-}
-input:focus,
-textarea:focus {
-  background: #fff8e4;
-  outline: 1px solid rgb(255, 199, 78);
-}
-label {
-  background: rgba(255, 255, 255, 0.8);
-  font-weight: bold;
-  font-size: 12px;
-}
-form {
-  padding: 1rem;
-  background: #333;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-}
-#submit {
-  width: 150px;
-  height: auto;
-  padding: 1rem;
-  margin: 1rem;
-  align-self: center;
-  border: none;
-  box-shadow: 0 7px 12px rgba(0, 0, 0, 0.8);
-  background: rgba(100, 255, 100, 0.8);
-  font-weight: 600;
-  font-size: 20px;
-  transition: 0.5s;
-  border-radius: 5px;
-}
-#submit:hover {
-  background: rgba(12, 124, 12, 0.8);
-  color: #fff;
-  border-radius: 15px;
-}
-#submit:disabled {
-  color: #555;
-  background: #aaa;
-  box-shadow: none;
-}
-#submit:disabled:hover {
-  border-radius: 5px;
-  background: #aaa;
-}
-</style>
+<style scoped></style>
